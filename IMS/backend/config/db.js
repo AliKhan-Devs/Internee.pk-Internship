451cb3e466ed72549a894f91b157c5fe004dd369
alikhan-devs <alikhandevs@gmail.com>
2025-10-01 14:42:12 +0500

    .

--- IMS/backend/config/db.js
+++ IMS/backend/config/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-import 'dotenv/config'; 
+import dotenv from 'dotenv';
+dotenv.config();
 
 const DB_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/IMS';
+
 const dbConnect = async ()=>{
     try {
-        console.log(DB_URI);
         await mongoose.connect(DB_URI);
         console.log('Mongo DB Connected Successfully')
     } catch (error) {
