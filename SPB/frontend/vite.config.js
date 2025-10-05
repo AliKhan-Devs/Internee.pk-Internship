93c78f3a28154ce879c1c40e2a0ac5194654bb61
alikhan-devs <alikhandevs@gmail.com>
2025-10-05 20:29:14 +0500

    Add PWA support and public assets to frontend
    Added PWA manifest, icons, and images to the public directory. Updated .gitignore to include .env. Installed vite-plugin-pwa and updated Vite config for PWA integration.

--- SPB/frontend/vite.config.js
+++ SPB/frontend/vite.config.js
@@ -9,7 +9,79 @@ export default defineConfig({
     host: true, // This is equivalent to --host and binds to 0.0.0.0
     port: 5173, // Optional: ensure the port is 5173
   },
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss(),
+
+
+  VitePWA({
+    registerType: 'autoUpdate', // automatically update SW
+    includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'], // add your icons here
+    manifest: {
+      name: 'PortaBuild',
+      short_name: 'PortaBuild',
+      description: 'The ultimate portfolio builder for developers & designers',
+      theme_color: '#0F172A',
+      background_color: '#FFFFFF',
+      display: 'standalone',
+      orientation: 'any',
+      start_url: '/',
+      "icons": [
+        {
+          "src": "icons/icon-48x48.png",
+          "sizes": "48x48",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-256x256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "icons/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ]
+    }
+
+  })
+
+
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
