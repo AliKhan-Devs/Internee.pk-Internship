8c204b2f1e3e20dd4e81f4970532a0af43cbd760
Ali Khan <164453693+Alikhan23M@users.noreply.github.com>
2025-10-06 19:57:06 +0500

    Update userControllers.js

--- IMS/backend/Controllers/userControllers.js
+++ IMS/backend/Controllers/userControllers.js
@@ -46,11 +46,11 @@ export const loginUser = async (req, res) => {
     return res.status(500).json('Error Occured please try agani');
   }
    // 4. Set HttpOnly cookie
-    res.cookie("token", token, {
+    res.cookie("token", jwtToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
   return res.status(200).json({ 
